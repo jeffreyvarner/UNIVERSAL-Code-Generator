a47fb467fe5ccae7b70d48060f91c25f50d2de3b
jeffreyvarner <jeff@curveos.com>
2013-02-01 17:31:33 -0500

    Initial Commit

---
+++ Universal/VLDocument.h
@@ -0,0 +1,13 @@
+//
+//  VLDocument.h
+//  Universal
+//
+//  Created by Jeffrey Varner on 2/1/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLDocument : NSPersistentDocument
+
+@end
---
+++ Universal/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Universal
+//
+//  Created by Jeffrey Varner on 2/1/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
